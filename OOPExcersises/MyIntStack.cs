80da9d70bdee4d4a13d6188bf2fa27b5d9ef0b37
Ariel Rojas <arielwagnerrojas@gmail.com>
2024-05-14 20:15:58 -0400

    feat: implemented exercise 1 OOP

---
+++ OOPExcersises/MyIntStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExcersises
+{
+    internal class MyIntStack
+    {
+        private int[] contents;
+        private int tos; // Top of the stack
+
+        public MyIntStack()
+        {
+            contents = new int[5];
+            tos = -1;
+        }
+
+        public void Push(int value)
+        {
+            if (tos >= contents.Length)
+            {
+                Console.WriteLine("It is not possible to add more data.");
+            }
+            else
+            {
+                contents[++tos] = value;
+            }
+        }
+
+        public int Pop()
+        {
+
+            if (tos < 0)
+            {
+                Console.WriteLine("None data exists.");
+
+                return 0;
+            }
+            else
+            {
+                int value = contents[tos--];
+
+                return value;
+            }
+        }
+
+        public void PrintStack()
+        {
+            if (tos < 0)
+            {
+                Console.WriteLine("None data exists.");
+            }
+            else
+            {
+                Console.WriteLine("Prints data in the Stack: ");
+
+                for (int count = tos; count >= 0; count--)
+                {
+                    Console.WriteLine(contents[count]);
+                }
+            }
+        }
+    }
+}
---
+++ OOPExcersises/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OOPExcersises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyIntStack myIntStack = new MyIntStack();
+            myIntStack.Push(1);
+            myIntStack.Push(2);
+            myIntStack.Push(3);
+            myIntStack.Push(4);
+            myIntStack.Push(5);
+
+            myIntStack.PrintStack();
+            Console.WriteLine($"Pop data: {myIntStack.Pop()}");
+            myIntStack.PrintStack();
+        }
+    }
+}
